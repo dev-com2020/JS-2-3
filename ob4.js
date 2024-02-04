d4109a513d3fa2f237917c58baca163e39ab0502
DEV-COM <kwp@tlen.pl>
2024-02-04 14:39:35 +0100

    14:39

---
+++ ob4.js
@@ -0,0 +1,31 @@
+const allowes = ["1234","pass","admin"];
+
+function passwordChecker(pass){
+    return allowes.includes(pass);
+}
+
+function login(password){
+    return new Promise((resolve,reject) => {
+        if (passwordChecker(password)){
+            resolve({
+                status: true
+            });
+        } else {
+            reject(
+                {status:false}
+            )
+        }
+    });
+}
+
+function checker(pass){
+    login(pass)
+    .then(token => {
+        console.log("Zatwierdzam!");
+        console.log(token);
+    })
+    .catch(value => {
+        console.log("Odrzucamy!");
+        console.log(value);
+    })
+}
