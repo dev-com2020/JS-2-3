305d5e1cf0387c0bcb7d042abac8373dd55f5035
DEV-COM <kwp@tlen.pl>
2024-02-04 14:45:27 +0100

    14:45

--- ob4.js
+++ ob4.js
@@ -1,31 +1,33 @@
-const allowes = ["1234","pass","admin"];
+const allowes = ["1234", "pass", "admin"];
 
-function passwordChecker(pass){
+function passwordChecker(pass) {
     return allowes.includes(pass);
 }
 
-function login(password){
-    return new Promise((resolve,reject) => {
-        if (passwordChecker(password)){
+function login(password) {
+    return new Promise((resolve, reject) => {
+        if (passwordChecker(password)) {
             resolve({
                 status: true
             });
         } else {
             reject(
-                {status:false}
+                { status: false }
             )
         }
     });
 }
 
-function checker(pass){
+function checker(pass) {
     login(pass)
-    .then(token => {
-        console.log("Zatwierdzam!");
-        console.log(token);
-    })
-    .catch(value => {
-        console.log("Odrzucamy!");
-        console.log(value);
-    })
+        .then(token => {
+            console.log("Zatwierdzam!");
+            console.log(token);
+        })
+        .catch(value => {
+            console.log("Odrzucamy!");
+            console.log(value);
+        })
 }
+
+checker("12345");
